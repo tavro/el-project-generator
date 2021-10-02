079479279c7388c3c839cc6d02a81f1a8004363f
Isak Horvath <48546805+tavro@users.noreply.github.com>
2021-10-02 23:25:25 +0200

    NEW: init html-css-javascript projects

--- emacs-project-generator.el
+++ emacs-project-generator.el
@@ -21,9 +21,32 @@
   ;; since a string gets evaluated
   (setq main "void main()\n{\n\n}\n"))
 
+;; Get html <head>-skeleton
+;; as string
+(defun write-html-head ()
+  (setq head "<head>\n<meta charset=\"UTF-8\">\n<title>Title</title>\n<link rel=\"stylesheet\" href=\"#\">\n<style></style>\n<script src=\"#\"></script>\n</head>\n\n"))
+
+;; Get html <body>-skeleton
+;; as string
+(defun write-html-body ()
+  (setq body "<body>\n<div class=\"main\">\n<h1>Header</h1>\n<p>Paragraph</p>\n</div>\n</body>\n"))
+
+;; Create custom project structure
+;; for html and css
+(defun init-html-css (project-name)
+  (mk-file project-name "index.html")
+  (write-region
+   (concat "<!DOCTYPE html>\n<html lang=\"en\">\n"
+	   (concat (write-html-head)
+		   (concat (write-html-body) "</html>")))
+   nil (concat project-name "/index.html"))
+  (mk-file (concat project-name "/stylesheet") "style.css")
+  (mk-dir (concat project-name "/img"))
+  (mk-dir (concat project-name "/js")))
+
 ;; Create custom project structure
 ;; for D lang
-(defun init-d-project (project-name)
+(defun init-d (project-name)
   (mk-file project-name "main.d")
   (write-region
    (concat "import std.stdio;\n\n" (write-main))
@@ -33,6 +56,7 @@
 ;; Create custom project structure
 ;; depending on given language
 (defun init-project-structure (project-name lang)
-  (when (string= lang "d") (init-d-project project-name)))
+  (when (string= lang "d") (init-d project-name))
+  (when (string= lang "html-css") (init-html-css project-name)))
 
-(init-project-structure "d-project" "d")
+(init-project-structure "website" "html-css")
